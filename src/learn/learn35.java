52c730690021c056c37ede69448482859ece9b21
lmxlove <2971243180@qq.com>
2022-11-02 01:08:39 +0800

    提交

--- src/learn/learn35.java
+++ src/learn/learn35.java
@@ -7,6 +7,6 @@ package learn;
 public class learn35 {
   public static void main(String[] args) {
 
-    System.out.println("jjjj");
+    System.out.println("jjjj");//ctrl alt down复制当前行，光标跳转到下一行ctrl alt up复制当前行，光标跳转到上一行
   }
 }
