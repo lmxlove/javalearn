1eca402691e94cfe34a90ab4e08bb939afbeab2d
lmxlove <2971243180@qq.com>
2022-11-02 22:06:11 +0800

    代码提交

---
+++ src/learn/learn43.java
@@ -0,0 +1,36 @@
+package learn;
+
+import java.util.Optional;
+
+/**
+ * @author <lmx>
+ * @since 2022/11/2 17:21
+ */
+public class learn43 {
+    public static void main(String[] args) {
+        String id = null;
+        Optional<Item1> item1 = ItemService1.getItemByid(id);
+        System.out.println(item1.orElse(new Item1()).name);
+    }
+
+    public static class ItemService1 {
+        public static Optional<Item1> getItemByid(String id) {
+            return Optional.ofNullable("1".equals(id) ? new Item1() : null);
+        }
+    }
+
+    static class Item1 {
+        private String name;
+        private String id;
+        //系统默认调用无参构造函数，前提是你的类是无参的，
+        // 否则会报错，和当前类自己没有定义一个构造函数
+
+//        public Item1(String name) {
+//            this.name = name;
+//        }
+
+//        public Item1() {
+//
+//        }
+    }
+}
---
+++ src/learn/learn44.java
@@ -0,0 +1,38 @@
+package learn;
+
+/**
+ * @author <lmx>
+ * @since 2022/11/2 17:03
+ */
+public class learn44 {
+    public static void main(String[] args) {
+        String id = null;
+        Item item = ItemService.getItemByid(id);
+//        a
+        if (item != null) {
+            System.out.println(item.name);
+        }
+        //b
+//        System.out.println(item.name);
+
+
+    }
+
+    private static class ItemService {
+        public static Item getItemByid(String id) {
+            //c
+//            return id.equals("1")?new Item("iphone13"):null;
+            //d   ac空指针 ad正常  bc空指针 bd空指针
+            return "1".equals(id) ? new Item("iphone13") : null;
+        }
+    }
+
+    static class Item {
+        private String name;
+        private String id;
+
+        public Item(String name) {
+            this.name = name;
+        }
+    }
+}
