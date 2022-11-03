cea73fad9eb7096183588837cffc4d2ef7b779a9
lmxlove <2971243180@qq.com>
2022-11-03 14:20:17 +0800

    feat:新增c属性

--- src/learn/learn45.java
+++ src/learn/learn45.java
@@ -10,7 +10,8 @@ public class learn45 {
   //变更代码，提交代码到github演示
   public static void main(String[] args) {
     int a = 123;
-    String b="123";
+    String b = "123";
+    int c = 145;
     System.out.println("提交代码到github" + a);
   }
 
