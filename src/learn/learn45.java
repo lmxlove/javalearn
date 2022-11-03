d0fc1db6dca46211da09475f915e8e9555e5684f
lmxlove <2971243180@qq.com>
2022-11-03 18:55:03 +0800

    feat:新增代码

--- src/learn/learn45.java
+++ src/learn/learn45.java
@@ -6,6 +6,13 @@ package learn;
  */
 public class learn45 {
 
+  /**
+   * @return
+   */
+  @Override
+  public String toString() {
+    return super.toString();
+  }
 
   //变更代码，提交代码到github演示
   public static void main(String[] args) {
@@ -14,7 +21,11 @@ public class learn45 {
     int c = 145;
     long d = 2L;
     char e = 'l';
-    System.out.println("提交代码到github" + a);
+    Integer integer=new Integer(2);
+    System.out.println("提交代码到github " + a);
+    Object o=new Object();
+    System.out.println(o.toString());
+    System.out.println(integer.toString());
   }
 
 }
