2a96acdad10696a23a1ecc0776b10aff9c5ae4c5
lmxlove <2971243180@qq.com>
2022-11-03 11:55:21 +0800

    feat: 新增a属性

--- src/learn/learn45.java
+++ src/learn/learn45.java
@@ -9,7 +9,8 @@ public class learn45 {
 
   //变更代码，提交代码到github演示
   public static void main(String[] args) {
-    System.out.println("提交代码到github");
+    int a = 123;
+    System.out.println("提交代码到github" + a);
   }
 
 }
