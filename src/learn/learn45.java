ba1331a9ff7e907097705060d6481d1ab1a1c4eb
lmxlove <2971243180@qq.com>
2022-11-03 11:47:34 +0800

    feat: 新增练习

---
+++ src/learn/learn45.java
@@ -0,0 +1,15 @@
+package learn;
+
+/**
+ * @author <lmx>
+ * @since 2022/11/3 11:27
+ */
+public class learn45 {
+
+
+  //变更代码，提交代码到github演示
+  public static void main(String[] args) {
+    System.out.println("提交代码到github");
+  }
+
+}
